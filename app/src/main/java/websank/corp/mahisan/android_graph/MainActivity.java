b560d2a8f59e307dd6e7b1c4cd080136a98a1d6d
Sanchit Goel <sanchit.mgoel007@gmail.com>
2016-02-27 22:01:40 +0530

    Update MainActivity.java

--- app/src/main/java/websank/corp/mahisan/android_graph/MainActivity.java
+++ app/src/main/java/websank/corp/mahisan/android_graph/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
