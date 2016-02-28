76476c5f62a82ba1a6dc1d7cb705847d7e065be4
Sanchit Goel <sanchit.mgoel007@gmail.com>
2016-02-28 19:39:54 +0530

    Update MainActivity.java

--- app/src/main/java/websank/corp/mahisan/android_graph/MainActivity.java
+++ app/src/main/java/websank/corp/mahisan/android_graph/MainActivity.java
@@ -8,7 +8,6 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
